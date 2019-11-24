4b296a2daebc22ab235e970338de7bc5eb0ec92f
Caleb Bassi <calebjbassi@gmail.com>
2019-11-24 13:42:56 -0800

    Convert basic_keys.py to osprey

--- misc/basic_keys.py
+++ misc/basic_keys.py
@@ -1,34 +1,40 @@
-from talon.voice import Context, press, Key
 import string
-from ..utils import normalise_keys, insert
-from ..config import config
 
-# Alphabet words are configurable in your config.json.  default is talon_alphabet_words:
+from osprey.voice import Context, press, insert
+
+
+def normalise_keys(dict):
+    normalised_dict = {}
+    for k, v in dict.items():
+        for cmd in k.split('|'):
+            normalised_dict[cmd] = v
+    return normalised_dict
+
+
 talon_alphabet_words = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip"
-# voicecode_alphabet_words: "arch brov char dell etch fomp goof hark ice jinx koop lug mowsh nerb ork pooch quosh rosh sun teak unks verge womp trex yang zooch"
 
-alpha_alt = config.get("alphabet_words", talon_alphabet_words).split()
+alpha_alt = talon_alphabet_words.split()
 
 alphabet = dict(zip(alpha_alt, string.ascii_lowercase))
 
-f_keys = {f"F {i}": f"f{i}" for i in range(1, 13)}
+f_keys = {f"F{i}": f"F{i}" for i in range(1, 13)}
 
 simple_keys = normalise_keys(
     {
-        "(crimp | lloyd)": "left",
-        "chris": "right",
-        "jeep": "up",
-        "( dune | doom )": "down",
-        "( backspace | junk )": "backspace",
-        "(delete | forward delete | scrap | spunk)": "delete",
-        "(space | skoosh)": "space",
-        "(tab | tarp)": "tab",
-        "( enter | shock )": "enter",
-        "( escape | randall )": "escape",
-        "home": "home",
-        "pagedown": "pagedown",
-        "pageup": "pageup",
-        "end": "end",
+        "crimp|lloyd": "Left",
+        "chris": "Right",
+        "jeep": "Up",
+        "dune|doom": "Down",
+        "backspace|junk": "Backspace",
+        "delete|forward delete|scrap|spunk": "Delete",
+        "space|skoosh": "Space",
+        "tab|tarp": "Tab",
+        "enter|shock": "Enter",
+        "escape|randall": "Escape",
+        "home": "Home",
+        "pagedown": "PageDown",
+        "pageup": "PageUp",
+        "end": "End",
     }
 )
 
@@ -38,26 +44,26 @@ symbols = normalise_keys(
         # keys to press on a standard US keyboard layout. Commands for keys that do
         # require modifiers (e.g. ``"caret": "^"`) should belong in
         # ``text/symbol.py``.
-        "(tick | back tick)": "`",
-        "(comma | ,)": ",",
-        "(dot | period)": ".",
-        "(semicolon | semi)": ";",
-        "(quote | quatchet)": "'",
-        "(square | L square | left square | left square bracket)": "[",
-        "(R square | right square | right square bracket)": "]",
-        "(slash | forward slash)": "/",
+        "tick|back tick": "`",
+        "comma|,": ",",
+        "dot|period": ".",
+        "semicolon|semi": ";",
+        "quote|quatchet": "'",
+        "square|L square|left square|left square bracket": "[",
+        "R square|right square|right square bracket": "]",
+        "slash|forward slash": "/",
         "backslash": "\\",
-        "(minus | dash)": "-",
-        "(equals | smaqual)": "=",
+        "minus|dash": "-",
+        "equals|smaqual": "=",
     }
 )
 
 modifiers = normalise_keys(
     {
-        "command": "cmd",
-        "(control | troll)": "ctrl",
-        "(shift | sky)": "shift",
-        "(alt | option)": "alt",
+        "command": "Cmd",
+        "control|troll": "Ctrl",
+        "shift|sky": "Shift",
+        "alt|option": "Alt",
     }
 )
 
@@ -69,7 +75,7 @@ keys.update(symbols)
 digits = {str(i): str(i) for i in range(10)}
 
 # separate arrow dictionary for combining with modifiers
-arrows = {"left": "left", "right": "right", "up": "up", "down": "down"}
+arrows = {"left": "Left", "right": "Right", "up": "Up", "down": "Down"}
 
 # map alnum and keys separately so engine gives priority to letter/number repeats
 keymap = keys.copy()
@@ -80,18 +86,18 @@ keymap.update(arrows)
 
 def get_modifiers(m):
     try:
-        return [modifiers[mod] for mod in m["basic_keys.modifiers"]]
+        return [modifiers[mod] for mod in m["modifiers"]]
     except KeyError:
         return []
 
 
 def get_keys(m):
     groups = [
-        "basic_keys.keys",
-        "basic_keys.arrows",
-        "basic_keys.digits",
-        "basic_keys.alphabet",
-        "basic_keys.keymap",
+        "keys",
+        "arrows",
+        "digits",
+        "alphabet",
+        "keymap",
     ]
     for group in groups:
         try:
@@ -120,21 +126,21 @@ def press_keys(m):
 
 
 ctx = Context("basic_keys")
-ctx.keymap(
-    {
-        "(uppercase | ship | sky) {basic_keys.alphabet}+ [(lowercase | lower | sunk)]": uppercase_letters,
-        "{basic_keys.modifiers}* {basic_keys.alphabet}+": press_keys,
-        "{basic_keys.modifiers}* {basic_keys.digits}+": press_keys,
-        "{basic_keys.modifiers}* {basic_keys.keys}+": press_keys,
-        "(go | {basic_keys.modifiers}+) {basic_keys.arrows}+": press_keys,
-        "number {basic_keys.digits}+ [over]": press_keys,
-        "tarsh": Key("shift-tab"),
-        "tarpy": [Key("tab"), Key("tab")],
-    }
-)
-ctx.set_list("alphabet", alphabet.keys())
-ctx.set_list("digits", digits.keys())
-ctx.set_list("keys", keys.keys())
-ctx.set_list("arrows", arrows.keys())
-ctx.set_list("modifiers", modifiers.keys())
-ctx.set_list("keymap", keymap.keys())
+ctx.set_keymap({
+    "(uppercase|ship|sky) {alphabet+}(lowercase|lower|sunk)?": uppercase_letters,
+    "{modifiers*}{alphabet+}": press_keys,
+    "{modifiers*}{digits+}": press_keys,
+    "{modifiers*}{keys+}": press_keys,
+    "(go|{modifiers+}) {arrows+}": press_keys,
+    "number {digits+}(over)?": press_keys,
+    "tarsh": lambda m: press("Shift-Tab"),
+    "tarpy": lambda m: press("Tab Tab"),
+})
+ctx.set_lists({
+    "alphabet": alphabet.keys(),
+    "digits": digits.keys(),
+    "keys": keys.keys(),
+    "arrows": arrows.keys(),
+    "modifiers": modifiers.keys(),
+    "keymap": keymap.keys(),
+})
