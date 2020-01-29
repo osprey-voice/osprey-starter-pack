99626496238a8c05a79c7512da3d4a8ef891d2b2
Caleb Bassi <calebjbassi@gmail.com>
2020-01-28 18:44:52 -0800

    copy symbols.py to basic_keys.py

--- src/misc/basic_keys.py
+++ src/misc/basic_keys.py
@@ -49,6 +49,42 @@ symbols = normalise_keys({
     "backslash": "\\",
     "minus|dash": "-",
     "equals": "=",
+
+    # simple
+    "(question [mark] | questo)": "?",
+    "plus": "+",
+    "tilde": "~",
+    "(bang | exclamation point | clamor)": "!",
+    "(dollar [sign] | dolly)": "$",
+    "(downscore | crunder)": "_",
+    "colon": ":",
+    "(lparen | [left] paren | precorp )": "(",
+    "(rparen | are paren | right paren | precose)": ")",
+    "(brace | left brace | kirksorp)": "{",
+    "(rbrace | are brace | right brace | kirkos)": "}",
+    "(angle | left angle | less than)": "<",
+    "(rangle | are angle | right angle | greater than)": ">",
+    "(star | asterisk)": "*",
+    "(pound | hash [sign] | octo | number sign)": "#",
+    "percent [sign]": "%",
+    "caret": "^",
+    "at sign": "@",
+    "(and sign | ampersand | amper)": "&",
+    "(pipe | spike)": "|",
+    "(dubquote | double quote | quatches)": '"',
+    # compound
+    "mintwice": "--",
+    "plustwice": "++",
+    "minquall": "-=",
+    "pluqual": "+=",
+    "starqual": "*=",
+    "triple quote": "'''",
+    "triple tick": "```",
+    "[forward] dubslash": "//",
+    "coal twice": "::",
+    "(dot dot | dotdot)": "..",
+    "(ellipsis | dot dot dot | dotdotdot)": "...",
+    # unnecessary: use repetition commands?
 })
 
 modifiers = normalise_keys({
