de80b6994cbb1603330557613a14dabb0f3ec27d
Caleb Bassi <calebjbassi@gmail.com>
2020-02-02 09:01:31 -0800

    formatters: add upper and lower

--- src/text/formatters.py
+++ src/text/formatters.py
@@ -2,6 +2,8 @@ from osprey.voice import Context, insert
 
 ctx = Context("formatters")
 ctx.set_rules({
+    "upper {word}": lambda m: insert(m['word'][0].capitalize()),
+    "lower {word}": lambda m: insert(m['word'][0].lower()),
     "word {word}": lambda m: insert(m['word'][0]),
     "(phrase|praise) {phrase}": lambda m: insert(m['phrase'][0]),
     "sentence {phrase}": lambda m: insert(m['phrase'][0][0].upper() + m['phrase'][0][1:]),
