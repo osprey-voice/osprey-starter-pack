468c90d19d729cc18a9e6f00538b6f7864c3db26
Caleb Bassi <calebjbassi@gmail.com>
2020-02-03 13:46:32 -0800

    refactor

--- src/misc/speech_toggle.py
+++ src/misc/speech_toggle.py
@@ -1,8 +1,8 @@
 from osprey.voice import Context, ContextGroup, default_context_group, preferred_phrases
 
-toggle_group = ContextGroup("toggle")
-toggle = Context("toggle", group=toggle_group)
-toggle.set_rules({
+ctx_group = ContextGroup("toggle")
+ctx = Context("toggle", group=ctx_group)
+ctx.set_rules({
     "sleep": lambda: default_context_group.disable(),
     "wake": lambda: default_context_group.enable(),
 })
