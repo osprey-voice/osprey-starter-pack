84f2337c36f5b7e294d8daff42b719ee1ad3e6c7
Caleb Bassi <calebjbassi@gmail.com>
2020-02-04 13:40:39 -0800

    fix callbacks without an argument

--- src/misc/osprey_control.py
+++ src/misc/osprey_control.py
@@ -3,8 +3,8 @@ from osprey.control import disable, quit_program
 
 ctx = Context("control")
 ctx.set_rules({
-    "disable": disable,
-    "quit": quit_program,
+    "disable": lambda m: disable(),
+    "quit": lambda m: quit_program(),
 })
 
 preferred_phrases.update({"disable", "quit"})
--- src/misc/speech_toggle.py
+++ src/misc/speech_toggle.py
@@ -3,8 +3,8 @@ from osprey.voice import Context, ContextGroup, default_context_group, preferred
 ctx_group = ContextGroup("toggle")
 ctx = Context("toggle", group=ctx_group)
 ctx.set_rules({
-    "sleep": lambda: default_context_group.disable(),
-    "wake": lambda: default_context_group.enable(),
+    "sleep": lambda m: default_context_group.disable(),
+    "wake": lambda m: default_context_group.enable(),
 })
 
 preferred_phrases.update({"sleep", "wake"})
