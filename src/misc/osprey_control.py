cdbd6bf0dcdd503c72458a6e16c577675693716e
Caleb Bassi <calebjbassi@gmail.com>
2020-02-04 11:27:37 -0800

    quit -> quit_program

--- src/misc/osprey_control.py
+++ src/misc/osprey_control.py
@@ -1,10 +1,10 @@
 from osprey.voice import Context, preferred_phrases
-from osprey.control import disable, quit
+from osprey.control import disable, quit_program
 
 ctx = Context("control")
 ctx.set_rules({
     "disable": lambda: disable(),
-    "quit": lambda: quit(),
+    "quit": lambda: quit_program(),
 })
 
 preferred_phrases.update({"disable", "quit"})
