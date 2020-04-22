3f15d80191db243fae8c0fc44394b3e15078e88e
Caleb Bassi <calebjbassi@gmail.com>
2020-04-22 10:49:03 -0700

    update formatters

--- src/core/formatters.py
+++ src/core/formatters.py
@@ -1,14 +1,20 @@
 import string
+import re
 
 from osprey.voice import Context, insert
+
+
+def uppercase_i(s):
+    return re.sub(r'\bi\b', 'I', s)
+
 
 ctx = Context('formatters')
 ctx.set_rules({
     # 'upper <word>': lambda m: insert(m['word'].capitalize()),
     # 'lower <word>': lambda m: insert(m['word'].lower()),
     # 'word <word>': lambda m: insert(m['word']),
-    'phrase <phrase>': lambda m: insert(m['phrase']),
+    'phrase <phrase>': lambda m: insert(uppercase_i(m['phrase'])),
     'title <phrase>': lambda m: insert(string.capwords(m['phrase'])),
-    'sentence <phrase>': lambda m: insert(m['phrase'].capitalize()),
+    'sentence <phrase>': lambda m: insert(uppercase_i(m['phrase'].capitalize())),
     'all caps <phrase>': lambda m: insert(m['phrase'].upper()),
 })
