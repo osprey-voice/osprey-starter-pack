48d8faba09a385b3a27d6991ada73accf78cccf1
Caleb Bassi <calebjbassi@gmail.com>
2020-04-22 16:12:19 -0700

    add repeat <n> more

--- src/core/repeater.py
+++ src/core/repeater.py
@@ -3,4 +3,5 @@ from osprey.voice import Context, repeat
 ctx = Context('repeater')
 ctx.set_rules({
     'repeat <n>': lambda m: repeat(m['n'] - 1),
+    'repeat <n> more': lambda m: repeat(m['n']),
 })
