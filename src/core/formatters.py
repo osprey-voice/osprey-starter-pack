84c528999bd51198cc757e4eed6c584b639d11f4
Caleb Bassi <calebjbassi@gmail.com>
2020-04-22 11:50:43 -0700

    add undo_insert

--- src/core/formatters.py
+++ src/core/formatters.py
@@ -1,7 +1,7 @@
 import string
 import re
 
-from osprey.voice import Context, insert
+from osprey.voice import Context, insert, press, repeat, undo_insert
 
 
 def uppercase_i(s):
@@ -13,8 +13,11 @@ ctx.set_rules({
     # 'upper <word>': lambda m: insert(m['word'].capitalize()),
     # 'lower <word>': lambda m: insert(m['word'].lower()),
     # 'word <word>': lambda m: insert(m['word']),
+
     'phrase <phrase>': lambda m: insert(uppercase_i(m['phrase'])),
     'title <phrase>': lambda m: insert(string.capwords(m['phrase'])),
     'sentence <phrase>': lambda m: insert(uppercase_i(m['phrase'].capitalize())),
     'all caps <phrase>': lambda m: insert(m['phrase'].upper()),
+
+    'undo insert': lambda m: undo_insert(),
 })
