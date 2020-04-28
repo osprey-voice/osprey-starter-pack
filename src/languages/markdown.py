dec3f8e9f861a45f0eb16d256b8e3380b15ea27a
Caleb Bassi <calebjbassi@gmail.com>
2020-04-28 10:12:41 -0700

    add code block

--- src/languages/markdown.py
+++ src/languages/markdown.py
@@ -1,6 +1,16 @@
-from osprey.voice import Context, insert
+from osprey.voice import Context, insert, press
+
+
+def code_block(m):
+    insert('```')
+    press('Enter')
+    press('Enter')
+    insert('```')
+    press('Up')
+
 
 ctx = Context('markdown')
 ctx.set_rules({
     'check box': lambda m: insert('- [ ] '),
+    'code block': code_block,
 })
