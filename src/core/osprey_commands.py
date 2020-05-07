c138e8cab47c9b6416b411cdc0aa135d4ca9c27e
Caleb Bassi <calebjbassi@gmail.com>
2020-05-07 12:13:52 -0700

    add 'reload scripts' command

--- src/core/osprey_commands.py
+++ src/core/osprey_commands.py
@@ -1,13 +1,15 @@
+from osprey.control import disable, quit_program, reload_scripts
+from osprey.open import open_config_dir, open_history_file, open_log_file
 from osprey.voice import Context
-from osprey.control import disable, quit_program
-from osprey.open import open_config_dir, open_log_file, open_history_file
 
 ctx = Context('osprey_commands')
 ctx.set_commands({
     'osprey disable': lambda m: disable(),
     'osprey quit': lambda m: quit_program(),
 
+    'reload scripts': lambda m: reload_scripts(),
+
     'open (config|configuration) (dir|directory)': lambda m: open_config_dir(),
-    'open log file': lambda m: open_log_file(),
     'open history file': lambda m: open_history_file(),
+    'open log file': lambda m: open_log_file(),
 })
