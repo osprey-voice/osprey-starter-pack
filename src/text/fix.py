0a694aeff3e1aebaa9f425ffb095b8a5f29a61e2
Caleb Bassi <calebjbassi@gmail.com>
2020-05-27 17:56:21 -0700

    add fix comment

--- src/text/fix.py
+++ src/text/fix.py
@@ -1,3 +1,8 @@
+"""
+This module is used for typing out words that are already in the model vocabulary but are difficult
+for the engine to identify because they get confused with other words.
+"""
+
 from osprey.voice import Context, insert
 
 fix_list = [
