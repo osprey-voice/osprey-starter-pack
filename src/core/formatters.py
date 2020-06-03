65ed405a78465832933cf26ba82f6bb68a27d5e4
Caleb Bassi <calebjbassi@gmail.com>
2020-06-03 09:27:14 -0700

    refactor formatters and add double under formatter

--- src/core/formatters.py
+++ src/core/formatters.py
@@ -8,16 +8,39 @@ def uppercase_i(s):
     return re.sub(r'\bi\b', 'I', s)
 
 
-def format(m, formatter):
-    words = m['phrase'].split(' ')
+def format(s, formatter):
+    words = s.split(' ')
     formatted_text = [formatter(i, word) for i, word in enumerate(words)]
     joined = ''.join(formatted_text)
-    insert(joined)
+    return joined
 
 
 def camel_case(m):
+    phrase = m['phrase']
     formatter = lambda i, word: word if i == 0 else word.capitalize()
-    format(m, formatter)
+    formatted = format(phrase, formatter)
+    insert(formatted)
+
+
+formatters_map = {
+    'all caps': lambda s: s.upper(),
+    'title': lambda s: string.capwords(s),
+
+    'kebab': lambda s: s.replace(' ', '-'),
+    'smash': lambda s: s.replace(' ', ''),
+    'snake': lambda s: s.replace(' ', '_'),
+    'double under': lambda s: '__{}__'.format(s.replace(' ', '_')),
+}
+
+
+def formatters(m):
+    phrase = m['phrase']
+    if 'title' in m['formatters']:
+        phrase = formatters_map['title'](phrase)
+        m['formatters'].remove('title')
+    for formatter in m['formatters']:
+        phrase = formatters_map[formatter](phrase)
+    insert(phrase)
 
 
 ctx = Context('formatters')
@@ -29,15 +52,11 @@ ctx.set_commands({
     'phrase <phrase>': lambda m: insert(uppercase_i(m['phrase'])),
     'sentence <phrase>': lambda m: insert(uppercase_i(m['phrase'].capitalize())),
 
-    'all caps <phrase>': lambda m: insert(m['phrase'].upper()),
     'camel <phrase>': camel_case,
-    'kebab <phrase>': lambda m: insert(m['phrase'].replace(' ', '-')),
-    'kebab title <phrase>': lambda m: insert(string.capwords(m['phrase']).replace(' ', '-')),
-    'smash <phrase>': lambda m: insert(m['phrase'].replace(' ', '')),
-    'smash title <phrase>': lambda m: insert(string.capwords(m['phrase']).replace(' ', '')),
-    'snake <phrase>': lambda m: insert(m['phrase'].replace(' ', '_')),
-    'snake all caps <phrase>': lambda m: insert(m['phrase'].replace(' ', '_').upper()),
-    'title <phrase>': lambda m: insert(string.capwords(m['phrase'])),
+    '<formatters>+ <phrase>': formatters,
 
     'undo insert': lambda m: undo_insert(),
+})
+ctx.set_choices({
+    'formatters': formatters_map.keys(),
 })
