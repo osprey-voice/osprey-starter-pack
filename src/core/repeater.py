d52487c6dcbdd98c0e4894696a77816a64f66c08
Caleb Bassi <calebjbassi@gmail.com>
2021-01-01 16:29:51 -0800

    add 'repeat it' command

--- src/core/repeater.py
+++ src/core/repeater.py
@@ -4,4 +4,5 @@ ctx = Context('repeater')
 ctx.set_commands({
     'repeat <n>': lambda m: repeat(m['n'] - 1),
     'repeat <n> more': lambda m: repeat(m['n']),
+    'repeat it': lambda m: repeat(1),
 })
